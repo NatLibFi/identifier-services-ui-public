558682d91d024bfa77cfd572f51cbd2e1f95262f
aatuny <27770287+aatuny@users.noreply.github.com>
2023-08-22 09:23:21 +0300

    Fix header to be lowercase because of express

--- src/server/proxy.js
+++ src/server/proxy.js
@@ -75,7 +75,7 @@ function preprocessRequest(proxyReqOpts, srcReq) {
   proxyReqOpts.headers['X-Forwarded-For'] = srcReq.ip;
 
   // Add originating IP to custom header, if it's defined
-  if(config.PROXY_CUSTOM_HEADER && config.PROXY_CUSTOM_HEADER.startsWith('X-')) {
+  if(config.PROXY_CUSTOM_HEADER && config.PROXY_CUSTOM_HEADER.startsWith('x-')) {
     proxyReqOpts.headers[config.PROXY_CUSTOM_HEADER] = srcReq.ip;
   }
 
